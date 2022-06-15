7ad9d9fdd2b4b4106af8ff99e5a47c79b00f2b0b
Jordi Florit <jordifloritensenyat03@gmail.com>
2022-06-15 16:08:41 +0200

    cambios

--- src/interficiegraficapraticafinal/Baralla.java
+++ src/interficiegraficapraticafinal/Baralla.java
@@ -11,28 +11,33 @@ import java.util.Random;
  * @author jordi
  */
 public class Baralla {
-    
+
     //La baralla es una array de cartes amb les cuals podem acceedir amb un index
     private Carta[] baralla;
-    private int total = 52;
+    private final int total = 52;
     private int ind;
 
     public Baralla() {
         baralla = new Carta[total];
         ind = 0;
     }
-    
+
     //Mètode setter per afegir cartes a la baralla
     public void setBaralla(Carta c) {
         baralla[ind++] = c;
     }
+
     //Mètode getter per treure una carta de la baralla
-    public Carta getCarta(int i){
+    public Carta getCarta(int i) {
         return baralla[i];
     }
-    
-    public int getTotal(){
+
+    public int getTotal() {
         return total;
+    }
+
+    public Carta repartir(int i) {
+        return baralla[i];
     }
 
     public void mesclar() {
@@ -51,24 +56,6 @@ public class Baralla {
             baralla[i] = baralla[azr];
             baralla[azr] = temp;
         }
-    }
-    
-    public Carta repartir(int i){
-        return baralla[i];
-    }
-
-    @Override
-    public String toString() {
-        Carta c;
-        String output = "";
-
-        for (int i = 0; i < ind; i++) {
-            c = baralla[i];
-            output += c.toString() + "\n";
-        }
-
-        return output;
-
     }
 
 }
